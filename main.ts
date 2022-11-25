be99781d4cc5a864a12802b03af838600e0c1aa4
rjoxford <r.j.oxford@hotmail.co.uk>
2022-11-25 12:56:52 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
 radio.onReceivedValue(function (name, value) {
-    if (name == "forward") {
+    if (name == "left") {
         if (value > 0) {
             kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, value)
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, value)
+        } else {
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(value))
         }
-        if (value < 0) {
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, value)
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, value)
+    }
+    if (name == "right") {
+        if (value > 0) {
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, value)
+        } else {
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(value))
         }
     }
 })
 basic.showLeds(`
-    . . # . .
-    . . # . .
-    # . # . #
     . # # # .
     . . # . .
+    # . # . #
+    # . # . #
+    . # # # .
     `)
