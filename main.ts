f81df04d76086b90344135359e89c988b045b27a
rjoxford <r.j.oxford@hotmail.co.uk>
2022-12-09 13:25:28 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
+function turn_only (num: number) {
+	
+}
+function drive_forward_and_turn (forward: number, turn: number) {
+	
+}
 radio.onReceivedValue(function (name, value) {
-    if (name == "left") {
+    if (name == "turn") {
         if (value > 0) {
             kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, value)
         } else {
             kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(value))
         }
     }
+    basic.showString("BREAK")
+    if (name == "forward") {
+    	
+    }
+    basic.showString("BREAK")
     if (name == "right") {
         if (value > 0) {
             kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, value)
@@ -14,10 +25,19 @@ radio.onReceivedValue(function (name, value) {
         }
     }
 })
+function drive_forward (num: number) {
+    if (num > 0) {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, num)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, num)
+    } else {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(num))
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(num))
+    }
+}
 basic.showLeds(`
     . # # # .
+    # # # # #
     . . # . .
-    # . # . #
     # . # . #
     . # # # .
     `)
