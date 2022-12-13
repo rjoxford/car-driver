ff5773e79129307fb5cce77d8bb314dd89559804
rjoxford <r.j.oxford@hotmail.co.uk>
2022-12-13 13:02:25 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-function turn_only (num: number) {
-    if (Math.abs(num) > 70) {
-        if (num < 0) {
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, num)
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, num)
+function turn_only (turn: number) {
+    if (Math.abs(turn) > 70) {
+        if (turn < 0) {
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, Math.abs(turn))
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(turn))
         }
-        if (num < 0) {
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, num)
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, num)
+        if (turn < 0) {
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(turn))
+            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, Math.abs(turn))
         }
     } else {
     	
@@ -14,19 +14,19 @@ function turn_only (num: number) {
 }
 function drive_forward_and_turn (forward: number, turn: number) {
     if (forward > 0) {
-        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, forward * ((0 - 0) / turn))
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, forward * ((turn_only_thresh - turn) / turn))
         kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, forward)
     } else {
-        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(forward))
-        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(forward))
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, Math.abs(forward))
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, Math.abs(forward) * ((turn_only_thresh - Math.abs(turn)) / Math.abs(turn)))
     }
 }
 radio.onReceivedValue(function (name, value) {
-    if (name == "turn") {
-        turn_only(value)
-    }
     if (name == "forward") {
-        drive_forward(value)
+        rforward = value
+    }
+    if (name == "turn") {
+        rturn = value
     }
 })
 function drive_forward (num: number) {
@@ -38,6 +38,9 @@ function drive_forward (num: number) {
         kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, Math.abs(num))
     }
 }
+let rturn = 0
+let rforward = 0
+let turn_only_thresh = 0
 basic.showLeds(`
     . # # # .
     # # # # #
@@ -45,3 +48,9 @@ basic.showLeds(`
     # . # . #
     . # # # .
     `)
+turn_only_thresh = 70
+loops.everyInterval(100, function () {
+    if (Math.abs(rturn) <= turn_only_thresh) {
+    	
+    }
+})
